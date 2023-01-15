612c99e3ee8fe088829e71beb4e033f3a5f65b93
Ivan <ivann.chen24@gmail.com>
2023-01-15 11:20:06 -0600

    On branch main 	new file:   .gitignore 	new file:   Main.java 	modified:   TimeCalc.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nWelcome. Please enter (in order) the following parameters. Hours of time1, Minutes of time1, Seconds of time1. (milliseconds should be added as a decimal value to seconds.) Hours of time2, Minutes of time2, Seconds of time2.");
+        int t1 = scan.nextInt();
+        int t2 = scan.nextInt();
+        double t3 = scan.nextDouble();
+        int t4 = scan.nextInt();
+        int t5 = scan.nextInt();
+        double t6 = scan.nextDouble();
+        System.out.println();
+        System.out.println(TimeCalc.addTime(t1,t2,t3,t4,t5,t6));
+    }
+}
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,29 +1,22 @@
 // This doesn't work yet, but I'm trying to make a class that can add and subtract time in hours, minutes, and seconds.
-public Class TimeCalc
+public class TimeCalc
 {
-    private int hour;
-    private int mins;
-    private int secs;
-    private int totalSecs;
 
-    public TimeCalc(int hour, int mins, int secs)
+    public static String addTime(int h1, int m1, double s1, int h2, int m2, double s2)
     {
-        this.hour = hour;
-        this.mins = mins;
-        this.secs = secs;
+        double time1 = h1*3600 + m1*60 + s1;
+        double time2 = h2*3600 + m2*60 + s2;
+        double totalSecs = time1+time2;
+        double hours = ((int)totalSecs)/3600;
+        totalSecs %= 3600;
+        double mins = ((int)totalSecs)/60;
+        totalSecs %= 60;
+        double secs = Math.round(totalSecs*100);
+        secs /= 100;
+        return ((int)hours + " hours, " + (int)mins + " minutes, and " + secs + " seconds.");
     }
 
-    
-
-    public int 
-
-    public void addTime(int hour, int mins, int secs)
-    {
-        this.hour += hour;
-        this.mins += mins;
-        this.secs += secs;
-    }
-
+    /* 
     public void subtractTime(int hour, int mins, int secs)
     {
         this.hour -= hour;
@@ -36,6 +29,6 @@ public Class TimeCalc
         totalSecs = (hour * 3600) + (mins * 60) + secs;
         return totalSecs;
     }
-
+    */
 
 }
